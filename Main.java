b9f283359707475b475d80844fd5e23c4b5bb677
Pedro Ramos <pedro@PC-Pedro>
2025-09-03 20:37:11 -0300

    Operacoes de insercoes pelo main feitas

--- Main.java
+++ Main.java
@@ -7,52 +7,75 @@ import java.util.ArrayList;
 
 public class Main{
 	public static void main(String[] args){
+		int[] cargasLimitadas = {100,3500,7000,10000,35000,70000,100000,250000,350000,500000};
+                for(int i = 0; i < cargasLimitadas.length; i++){
+			int limite = cargasLimitadas[i];
+			long soma = 0;
+
+			for(int j = 0; j < 15; j++){
+				ArvoreSplay tree = new ArvoreSplay(cargasLimitadas[i]);
+                        	try (BufferedReader reader = new BufferedReader(new FileReader("cargas/saida.txt"))) {
+                                	String linha;
+                                	int contador = 0;
+					
+					long inicio = System.nanoTime();
+                                	while ((linha = reader.readLine()) != null && contador < limite) {
+                                        	int numero = Integer.parseInt(linha);
+                                        	tree.add(numero);
+                                        	contador++;
+                                	}
+					long fim = System.nanoTime();
+					soma += (fim-inicio);
+                        	} catch (IOException e) {
+                                	System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+                        	}
+                        	System.out.println("Total lido");
+			}
+			long media = soma/15;
+			try (BufferedWriter writer = new BufferedWriter(new FileWriter("resultadosTestes/insercaoCacheNaoCheioSplay.txt", true))) {
+                        	writer.write(Long.toString(media));
+                        	writer.newLine();
+                	} catch (IOException e) {
+                        	System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+                	}
+                }
+
 		int[] cargas = {1000,35000,70000,100000,350000,700000,1000000,2500000,3500000,5000000};
 		for(int i = 0; i < cargas.length; i++){
-			ArvoreSplay tree = new ArvoreSplay(cargas[i]);
+			int capacidadeCache = (int) (cargas[i]*0.1);
 			int limite = cargas[i];
+			long soma = 0;
 
-        		try (BufferedReader reader = new BufferedReader(new FileReader("cargas/saida.txt"))) {
-            			String linha;
-            			int contador = 0;
+			 for(int j = 0; j < 15; j++){
+                                ArvoreSplay tree = new ArvoreSplay(capacidadeCache);
+                                try (BufferedReader reader = new BufferedReader(new FileReader("cargas/saida.txt"))) {
+                                        String linha;
+                                        int contador = 0;
 
-            			while ((linha = reader.readLine()) != null && contador < limite) {
-                			int numero = Integer.parseInt(linha);
-					insercaoCacheNaoCheio(tree, numero);
-                			contador++;
-            			}
-        		} catch (IOException e) {
-            			System.out.println("Erro ao ler o arquivo: " + e.getMessage());
-        		}
-
-        		System.out.println("Total lido");
+                                        long inicio = System.nanoTime();
+                                        while ((linha = reader.readLine()) != null && contador < limite) {
+                                                int numero = Integer.parseInt(linha);
+                                                tree.add(numero);
+                                                contador++;
+                                        }
+                                        long fim = System.nanoTime();
+                                        soma += (fim-inicio);
+                                } catch (IOException e) {
+                                        System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+                                }
+                                System.out.println("Total lido");
+                        }
+                        long media = soma/15;
+                        try (BufferedWriter writer = new BufferedWriter(new FileWriter("resultadosTestes/insercaoCacheCheioSplay.txt", true))) {
+                                writer.write(Long.toString(media));
+                                writer.newLine();
+                        } catch (IOException e) {
+                                System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+			}
 		}
-	}
-
-	public static void insercaoCacheNaoCheio(ArvoreSplay tree, int v){
-		long soma = 0;
-		
-		for(int i = 0; i < 15; i++){
-			long inicio = System.nanoTime();
-			tree.add(v);
-			long fim = System.nanoTime();
-			soma += (fim-inicio);
-		}
-
-		long media = soma/15;
-
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter("resultadosTestes/insercaoSimples.txt", true))) {
-        		writer.write(Long.toString(media));
-        		writer.newLine();
-    		} catch (IOException e) {
-        		System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
-    		}
+				
 	}
 	
-	public static void insercaoCacheCheio(){
-	
-	}
-
 	public static void searchPresente(){
 	
 	}
