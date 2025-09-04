95bd95dffe0748981b1e320fbbd61dd74c0d0f9b
Pedro Ramos <pedro@PC-Pedro>
2025-09-04 03:10:33 -0300

    Main feito

--- Main.java
+++ Main.java
@@ -4,11 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main{
 	public static void main(String[] args){
 		int[] cargasLimitadas = {100,3500,7000,10000,35000,70000,100000,250000,350000,500000};
-                for(int i = 0; i < cargasLimitadas.length; i++){
+                int[] cargas = {1000,35000,70000,100000,350000,700000,1000000,2500000,3500000,5000000};
+		
+		for(int i = 0; i < cargasLimitadas.length; i++){
 			int limite = cargasLimitadas[i];
 			long soma = 0;
 
@@ -29,7 +32,6 @@ public class Main{
                         	} catch (IOException e) {
                                 	System.out.println("Erro ao ler o arquivo: " + e.getMessage());
                         	}
-                        	System.out.println("Total lido");
 			}
 			long media = soma/15;
 			try (BufferedWriter writer = new BufferedWriter(new FileWriter("resultadosTestes/insercaoCacheNaoCheioSplay.txt", true))) {
@@ -40,7 +42,6 @@ public class Main{
                 	}
                 }
 
-		int[] cargas = {1000,35000,70000,100000,350000,700000,1000000,2500000,3500000,5000000};
 		for(int i = 0; i < cargas.length; i++){
 			int capacidadeCache = (int) (cargas[i]*0.1);
 			int limite = cargas[i];
@@ -63,7 +64,6 @@ public class Main{
                                 } catch (IOException e) {
                                         System.out.println("Erro ao ler o arquivo: " + e.getMessage());
                                 }
-                                System.out.println("Total lido");
                         }
                         long media = soma/15;
                         try (BufferedWriter writer = new BufferedWriter(new FileWriter("resultadosTestes/insercaoCacheCheioSplay.txt", true))) {
@@ -73,26 +73,111 @@ public class Main{
                                 System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
 			}
 		}
-				
-	}
-	
-	public static void searchPresente(){
-	
-	}
 
-	public static void searchNaoPresente(){
-	
-	}
+		for(int i = 0; i < cargasLimitadas.length; i++){
+                        int limite = cargasLimitadas[i];
+                        long somaPresente = 0;
+			long somaAusente = 0;
+			long somaUniforme = 0;
+			long somaScan = 0;
+			long somaZipf = 0;
 
-	public static void searchUniforme(){
-	
-	}
+                        ArvoreSplay tree = new ArvoreSplay(cargasLimitadas[i]);
+                        ArrayList<Integer> elementos = new ArrayList<>();
+			int ultimoNum = 0;
+                        int numInexistente = -1;
 
-	public static void searchScanSequencial(){
-	
-	}
+			try (BufferedReader reader = new BufferedReader(new FileReader("cargas/saida.txt"))) {
+                        	String linha;
+                                int contador = 0;
 
-	public static void zipf(){
-	
+                                while ((linha = reader.readLine()) != null && contador < limite) {
+                                	int numero = Integer.parseInt(linha);
+                                        tree.add(numero);
+					elementos.add(numero);
+                                        contador++;
+					ultimoNum = numero;
+                                }
+			}catch (IOException e) {
+                                System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+                        }
+					
+			for(int j = 0; j < 15; j++){
+				long inicio = System.nanoTime();
+				tree.search(ultimoNum);
+				long fim = System.nanoTime();
+                                somaPresente += (fim-inicio);
+			}
+
+			for(int j = 0; j < 15; j++){
+                                long inicio = System.nanoTime();
+                                tree.search(numInexistente);
+                                long fim = System.nanoTime();
+                                somaAusente += (fim-inicio);
+                        }
+
+			Random rand = new Random();
+			for(int j = 0; i < 15; j++){
+				int elementoAleatorio = elementos.get(rand.nextInt(elementos.size()));
+				long inicio = System.nanoTime();
+				tree.search(elementoAleatorio);
+				long fim = System.nanoTime();
+				somaUniforme += (fim-inicio);
+			}
+
+			for(int j = 0; j < 15; j++){
+				long inicio = System.nanoTime();
+				for(int numero: elementos)
+				tree.search(numero);
+				long fim = System.nanoTime();
+				somaScan += (fim-inicio);
+			}
+
+			double s = 1.0;
+			int n = elementos.size();
+			double[] probs = new double[n];
+			double somaP = 0.0;
+			for(int k = 1; k <= n; k++){
+				probs[k-1] = 1.0 / Math.pow(k,s);
+				somaP += probs[k-1];
+			}
+			for(int k = 0; k < n; k++) probs[k] /= somaP;
+			for(int j = 0; j < 15; j++){
+				long inicio = System.nanoTime();
+				for(int acesso = 0; acesso < n; acesso++){
+					double r = rand.nextDouble();
+					double acumulado = 0.0;
+					for(int k = 0; k < n; k++){
+						acumulado += probs[k];
+						if(r <= acumulado){
+							tree.search(elementos.get(k));
+							break;
+						}
+					}
+				}
+				long fim = System.nanoTime();
+				somaZipf += (fim-inicio);
+			}	
+                 
+                        long mediaPresente = somaPresente/15;
+			long mediaAusente = somaAusente/15;
+			long mediaUniforme = somaUniforme/15;
+			long mediaScan = somaScan/15;
+			long mediaZipf = somaZipf/15;
+
+			gravarResultado("resultadosTestes/buscaPresenteSplay.txt", mediaPresente);
+            		gravarResultado("resultadosTestes/buscaNaoPresenteSplay.txt", mediaAusente);
+            		gravarResultado("resultadosTestes/buscaUniformeSplay.txt", mediaUniforme);
+            		gravarResultado("resultadosTestes/buscaScanSplay.txt", mediaScan);
+            		gravarResultado("resultadosTestes/buscaZipfSplay.txt", mediaZipf);
+                        }
+        }				
+	private static void gravarResultado(String arquivo, long valor) {
+        	try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo, true))) {
+            		writer.write(Long.toString(valor));
+            		writer.newLine();
+        	} catch (IOException e) {
+            		System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+        	}
 	}
 }
